f3a1bb5cc8b31d841f7aed851fedd29f4e3f354f
J-Bourgeais <bourgeaisjulie@gmail.com>
2025-10-14 08:46:56 +0200

    Idées début TP4

--- tp4/tp.c
+++ tp4/tp.c
@@ -1,6 +1,28 @@
 /* GPLv2 (c) Airbus */
 #include <debug.h>
+#include <pagemem.h>
+
+
+/*Pour l'instant j'écris des idées, à compléter et corriger avec la correction*/
+
 
 void tp() {
 	// TODO
+	//Q1
+	printf("cr3 :",get_cr3());
+
+	//Q2
+	 //cf pagemem.h
+	pde32_t* pgd = (pde32_t*)0x600000;
+	set_cr3((uint32_t)pgd);
+
+	//Q3 : Tester
+
+	//Q4
+	pte32_t* ptb = (pte32_t*)0x601000;
+
+	//Q5
+	//I don't know
+
+
 }
